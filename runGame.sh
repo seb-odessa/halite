0f479796bbd76d00ae9ccdc6378d68ee41891814
seb-odessa <seb@ukr.net>
2016-11-19 12:58:20 +0200

    local changes

--- runGame.sh
+++ runGame.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 TARGET=release
-
+rm *hlt
 cargo build --release
-~/bin/halite -d "40 40" "target/release/MyBot" "target/release/MyBotV6" "target/release/MyBotV5"
+~/bin/halite -d "30 30" "target/release/MyBot" "target/release/MyBotV7" "target/release/MyBotV6"
